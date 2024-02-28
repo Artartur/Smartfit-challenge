6ae8a8cac04a69800eaf2c819f0227d90ab7ad0e
artartur <arturgfs@hotmail.com>
2024-02-27 23:08:35 -0300

    Edit some constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,7 @@ export enum Footer {
 
 export enum Form {
   AFTERNOON = 'Tarde',
-  AFTERNOON_PERIOD = '12:01 às 18:00',
+  AFTERNOON_PERIOD = '12:00 às 18:00',
   CLEAR = 'Limpar',
   CLOSED_UNITS = 'Exibir unidades fechadas',
   FIND_UNITS = 'Encontrar unidade',
@@ -16,7 +16,7 @@ export enum Form {
   MORNING_PERIOD = '06:00 às 12:00',
   MSG_TRAINGING_PERIOD = 'Qual período quer treinar?',
   NIGHT = 'Noite',
-  NIGHT_PERIOD = '18:01 às 23:00',
+  NIGHT_PERIOD = '18:00 às 23:00',
   TIME = 'Horário',
 }
 
